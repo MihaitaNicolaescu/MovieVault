474792225ebaff9ef45455182a481e4abdb13c24
MindOfBear <ovidiunoc@gmail.com>
2025-01-03 02:29:54 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="text-3xl font-bold text-red-500">
+      Hello world a!
+    </p>
+    </>
+  )
+}
+
+export default App
