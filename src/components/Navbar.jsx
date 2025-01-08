7613ac8654154fe59822b531de9b777dedd841e3
MihaitaNicolaescu <mihaita_nicoalescu@yahoo.com>
2025-01-08 23:04:37 +0200

    Implement search movie and fetch data from endpoint

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ function Navbar({setMovies, movies}) {
     return (
         <div id="navbar" className="grid grid-cols-3 bg-purple-800 h-16 content-center rounded-md w-1/2">
             <div>
-                <p className="block mb-2 text-2xl content-center font-extrabold text-white p-1 font-sans">movieVault🗄️</p>
+                <p className="block mb-2 text-2xl content-center font-extrabold text-white p-1 font-sans">movieVault 🗄️</p>
             </div>
             <div className="flex items-center justify-center">
                 <input type="text" id="search" className="rounded-lg w-full py-2 px-3 text-white bg-purple-600 placeholder:text-gray-300" placeholder="Search movie" value={searchMovie} onChange={(e) => {setSearchMovie(e.target.value)}} />
